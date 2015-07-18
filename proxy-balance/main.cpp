58ba39270c84330befb0914d1cc54c1115576c45
reddec <net.dev@mail.ru>
2015-07-18 21:22:55 +0300

    Typo fix

--- proxy-balance/main.cpp
+++ proxy-balance/main.cpp
@@ -5,7 +5,7 @@
 
 int main(int argc, const char **argv) {
     TCLAP::CmdLine cmd("Broker balance module", ' ', "1.0.0", true);
-    TCLAP::ValueArg<std::string> common_broker_url("u", "url", "Broker url", false, "tcp://127.0.0.10000",
+    TCLAP::ValueArg<std::string> common_broker_url("u", "url", "Broker url", false, "tcp://127.0.0.1:10000",
                                                    "zmq bind");
     TCLAP::ValueArg<std::string> proxy_backend("b", "backend", "Backend binding for proxy", false,
                                                "tcp://127.0.0.1:10001",
